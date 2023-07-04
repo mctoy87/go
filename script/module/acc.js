9529b00c39bf77720b326c700e32ec97194cb170
mctoy87 <mctoy87@mail.ru>
2023-07-04 15:47:21 +0200

    add modal form and accordeon

---
+++ script/module/acc.js
@@ -0,0 +1,46 @@
+const items = document.querySelectorAll('.faq__item');
+const buttons = document.querySelectorAll('.faq__button');
+const textWrapper = document.querySelectorAll('.faq__item_text-wrap');
+const navBtn = document.querySelector('.navigation__link-order');
+const modal = document.querySelector('.modal');
+
+console.log('navBtn :', navBtn);
+console.log('items :', items);
+console.log('buttons :', buttons);
+
+let heightWrapper = 0;
+textWrapper.forEach(elem => {
+  if (heightWrapper < elem.scrollHeight) {
+    heightWrapper = elem.scrollHeight;
+  }
+})
+
+buttons.forEach((btn, index) => {
+  btn.addEventListener('click', ()=> {
+    for (let i = 0; i < items.length; i++) {
+      if (index === i) {
+        textWrapper[i].style.height =
+          items[i].classList.contains('faq__item_active') ?
+          '' : `${heightWrapper}px`;
+
+        items[i].classList.toggle('faq__item_active');
+        btn.matches('.faq__button_purple') ?
+          btn.classList.add('faq__button_purple-active') :
+          btn.classList.add('faq__button_white-active');
+      } else {
+        items[i].classList.remove('faq__item_active');
+        textWrapper[i].style.height = '';
+      }
+    }
+  });
+});
+
+navBtn.addEventListener('click', () => {
+  modal.classList.add('modal_open');
+});
+
+modal.addEventListener('click', (e) => {
+  if (e.target===modal || e.target.closest('.modal__close')) {
+    modal.classList.remove('modal_open');
+  }
+});
---
+++ script/script.js
@@ -0,0 +1,1 @@
+import './module/acc.js';
