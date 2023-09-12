2fee063136f2717f160beae07d6a3a15460dd4b5
mctoy87 <mctoy87@mail.ru>
2023-09-12 11:00:27 +0200

    added adaptive for slider

--- script/module/swiper.js
+++ script/module/swiper.js
@@ -7,9 +7,34 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+
+  breakpoints: {
+    320: {
+      centeredSlidesBounds: true,
+      // slidesPerView: 2,
+      // spaceBetween: 20
+    },
+    480: {
+      // slidesPerView: 3,
+      // spaceBetween: 30
+    },
+    1380: {
+      // centeredSlidesBounds: true,
+      // slidesPerView: 4,
+      // spaceBetween: 40
+    }
+  }
 });
 
 
 swiper.slides.forEach(element => {
   element.style.display = 'flex';
 });
+
+// const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+// if(viewportWidth < 480) {
+//   const slider = document.querySelectorAll('.reviews__item');
+//   console.log(slider);
+// }
+// console.log(window.innerWidth);
+// console.log(document.documentElement.clientWidth);
